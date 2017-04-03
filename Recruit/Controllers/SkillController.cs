74cd70561d35520bec48aa860344e0accd78707e
Sakshi Khandelwal <sakshi_khandelwal@dell.com>
2017-04-03 17:12:07 +0530

    skill view changed

---
+++ Recruit/Controllers/SkillController.cs
@@ -0,0 +1,21 @@
+﻿using Recruit.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Recruit.Controllers
+{
+    public class SkillController : Controller
+    {
+        // GET: Skill
+        public ActionResult EditSkill()
+        {
+            List<Skills> obj;
+            using(var context = new ApplicationDbContext())
+            {
+                obj = context.Skills.ToList();
+            }
+            return View(obj);
+        }
+    }
+}
